ac35fded242a72c3f88775f7bb5645f005be7450
Cường Nguyễn Mạnh <cuongmanhnguyenz333@gmail.com>
2025-04-02 18:53:09 +0700

    up fontend demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+  
+    HomeComponent,
+            HeaderComponent,
+            FooterComponent,
+            OrderComponent,
+            DetailProductComponent,
+            OrderConfirmComponent,
+            LoginComponent,
+            RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+   bootstrap: [//HomeComponent,
+    //DetailProductComponent,
+    //OrderComponent,
+    //OrderConfirmComponent,
+    //LoginComponent,
+    RegisterComponent
+    ]
 })
 export class AppModule { }
